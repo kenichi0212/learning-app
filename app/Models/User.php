55acf3f4d73c1f67b5a2d9f38afb983bf3b418fc
kenichi0212 <siid.kt.0212@gmail.com>
2026-01-03 12:01:03 +0900

    メール認証の有効化とメール本文の日本語化

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// 追加：メールの日本語化に必要なクラス
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +47,21 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * メール認証通知を日本語で送信する
+     */
+    public function sendEmailVerificationNotification()
+    {
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('メールアドレスの確認')
+                ->line('下のボタンをクリックして、メールアドレスを確認してください。')
+                ->action('メールアドレスを確認', $url)
+                ->line('もし心当たりがない場合は、このメールを無視してください。');
+        });
+
+        $this->notify(new VerifyEmail);
     }
 }
