561177f5e101dcad187fe6382686db02a1abb9b8
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-01-09 00:36:04 +0900

    プロフィール画面にカメラ・スクショ許可設定機能を追加

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -27,6 +27,12 @@
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    @include('profile.partials.update-camera-settings-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
