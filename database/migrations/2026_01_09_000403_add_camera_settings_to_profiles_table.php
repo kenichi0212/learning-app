56b02f568284b26e23613b24ba3562c86aa16e83
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-01-09 20:15:39 +0900

    profilesにカラムの追加

--- database/migrations/2026_01_09_000403_add_camera_settings_to_profiles_table.php
+++ database/migrations/2026_01_09_000403_add_camera_settings_to_profiles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('profiles', function (Blueprint $table) {
-            //
+            $table->boolean('is_camera_enabled')->default(true)->after('user_id');
+        $table->boolean('is_screenshot_enabled')->default(true)->after('is_camera_enabled');
         });
     }
 
@@ -22,7 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('profiles', function (Blueprint $table) {
-            //
+            // ロールバック（取り消し）した時にカラムを削除するように設定
+        $table->dropColumn(['is_camera_enabled', 'is_screenshot_enabled']);
         });
     }
 };
