374dbd60b4406c47a28a3d4d2b6ae67f92a01a80
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-01-20 22:45:35 +0900

    display_nameカラムの削除

--- database/migrations/2026_01_04_140444_create_profiles_table.php
+++ database/migrations/2026_01_04_140444_create_profiles_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('display_name');
             $table->text('biography')->nullable();
             $table->string('image_path')->nullable();
             $table->timestamps();
