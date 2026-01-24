5e504c5c9207f0c5db391441fef7da9d2837cff9
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-01-24 22:39:35 +0900

    refactor: 共通のレイアウト構造をPageContainerコンポーネントに抽出

---
+++ resources/views/components/page-container.blade.php
@@ -0,0 +1,5 @@
+<div class="py-12 px-4 bg-gradient-to-br from-slate-900 via-blue-900 to-indigo-900 min-h-screen">
+    <div class="max-w-7xl mx-auto xl:px-6 lg:px-8">
+        {{ $slot }}
+    </div>
+</div>
