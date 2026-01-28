c7a0aa8d1d71b9600d15c264ef13da4191763608
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-01-28 13:10:26 +0900

    logoの微修正

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="flex justify-center items-center">
-    <img {{ $attributes->merge(['class' => 'h-9 w-auto']) }} src="{{ asset('img/logo.png') }}" alt="logo">
+    <img {{ $attributes->merge(['class' => 'h-9 w-auto']) }} src="{{ asset('img/logo.svg') }}" alt="logo">
 </div>
